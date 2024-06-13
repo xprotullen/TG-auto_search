ce7b6112a9908e11b155722da87869d4925719be
Protullen <138602811+protullen@users.noreply.github.com>
2024-06-13 10:55:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,13 +1,6 @@
 # (c) TheLx0980
-
 from pyrogram import filters, Client, enums
-import logging
-logger = logging.getLogger(__name__)
 
-@Client.on_message(filters.command("help") & filters.private & filters.incoming)
+@Client.on_message(filters.command("help") & filters.private)
 async def start(client, message):
-    await message.reply(
-        text="Hi, I'm Online",
-        disable_web_page_preview=True,
-        quote=True
-    )
+    await message.reply("Hi, I'm Online")
