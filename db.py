16268546adf4b36dd1ce400ade92ca98ea4706d0
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-11-01 03:39:36 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,15 +1,30 @@
-# db.py
-import motor.motor_asyncio
-from dotenv import load_dotenv
+from pymongo import MongoClient
 import os
 
-load_dotenv()
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://<username>:<password>@cluster.mongodb.net/")
+DB_NAME = "MovieBotDB"
 
-MONGO_URL = os.getenv("MONGO_URL")
-DB_NAME = os.getenv("DB_NAME", "MovieBot")
+client = MongoClient(MONGO_URL)
+db = client[DB_NAME]
+movies_col = db["movies"]
 
-mongo_client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
-db = mongo_client[DB_NAME]
+# ---------------- Functions ---------------- #
 
-# Collections
-cache_col = db["search_cache"]
+def save_movie(chat_id, link, caption):
+    movie_data = {
+        "chat_id": chat_id,
+        "link": link,
+        "caption": caption
+    }
+    movies_col.insert_one(movie_data)
+
+def delete_chat_data(chat_id):
+    result = movies_col.delete_many({"chat_id": chat_id})
+    return result.deleted_count
+
+def get_movies(chat_id, query):
+    # Case-insensitive search in captions
+    return list(movies_col.find({
+        "chat_id": chat_id,
+        "caption": {"$regex": query, "$options": "i"}
+    }))
