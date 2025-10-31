51ae60e4697290cd1571c7b608feba8a87a150ee
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-11-01 03:32:08 +0530

    Update search_msg.py

--- plugins/search_msg.py
+++ plugins/search_msg.py
@@ -1,37 +1,25 @@
 import re
-from pyrogram import filters, enums, Client
-from pyrogram.types import CallbackQuery, Message
-from search_info import DATABASE, send_result_message, extract_movie_details
-from database import get_indexes
-
-MEDIA_FILTER = enums.MessagesFilter.VIDEO 
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from database import search_movies
+from search_info import send_result_message
 
 @Client.on_message(filters.group & filters.text)
-async def search_movies(client: Client, message: Message):
-    if re.findall(r"(^[/!.,]|^[\U0001F600-\U000E007F])", message.text):
+async def search_movie_handler(client: Client, message: Message):
+    text = message.text.strip()
+
+    if re.match(r"(^[/!.,]|^[\U0001F600-\U000E007F])", text):
         return
 
-    if len(message.text) < 3:
+    if len(text) < 3:
         return
 
-    query = message.text
-    group_id = message.chat.id
-    indexed_chats = await get_indexes(group_id)
+    results = await search_movies(message.chat.id, text)
+    if not results:
+        return await message.reply_text("😔 No results found in database.")
 
-    if not indexed_chats:
-        await message.reply_text("❌ No indexed chats found. Use `/index chat_id` first.", quote=True)
-        return
+    movies = []
+    for doc in results:
+        movies.append(f"<b>{doc['movie_name']}</b>\n<b>Link:</b> {doc['link']}")
 
-    msgs = []
-    for chat_id in indexed_chats:
-        async for msg in client.USER.search_messages(chat_id, query=query, filter=MEDIA_FILTER):
-            if not msg.caption:
-                continue
-            movie_name, year, quality = extract_movie_details(msg.caption)
-            movie_text = f"<b>{movie_name} ({year}) {quality}</b>\n<b>Link:</b> {msg.link}"
-            msgs.append(movie_text)
-
-    if not msgs:
-        return await message.reply_text("😞 No results found.", quote=True)
-
-    await send_result_message(client, message, query, msgs, page=1)
+    await send_result_message(client, message, text, movies, page=1)
