da546331e08af05171b91084efc3aafa96ce630a
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-11-02 09:40:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,26 +7,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# ---------------- BASIC CONFIG ---------------- #
 id_pattern = re.compile(r"^\\d+$")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 APP_ID = int(os.getenv("APP_ID", "0"))
 API_HASH = os.getenv("API_HASH")
 USER_SESSION = os.getenv("USER_SESSION")
-
-# ---------------- DATABASE CONFIG ---------------- #
 MONGO_URL = os.getenv("MONGO_URL")
 DB_NAME = os.getenv("DB_NAME", "MovieBotDB")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "movies")
-
-# ---------------- REDIS CONFIG ---------------- #
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 REDIS_USERNAME = os.getenv("REDIS_USERNAME", None)
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 
-# ---------------- LOGGING SETUP ---------------- #
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
