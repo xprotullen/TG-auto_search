2a24a4aed67695f458e2b559e45571103171e282
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-11-02 09:27:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,30 +1,40 @@
 import os
-import logging
-import time
 import re
+import time
+import logging
 from logging.handlers import RotatingFileHandler
+from dotenv import load_dotenv
 
-id_pattern = re.compile(r'^.\d+$')
+load_dotenv()
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", ""))
-API_HASH = os.environ.get("API_HASH", "") 
-USER_SESSION = os.environ.get("USER_SESSION", "")
-VERIFY = {}
+# ---------------- BASIC CONFIG ---------------- #
+id_pattern = re.compile(r"^\\d+$")
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+APP_ID = int(os.getenv("APP_ID", "0"))
+API_HASH = os.getenv("API_HASH")
+USER_SESSION = os.getenv("USER_SESSION")
 
+# ---------------- DATABASE CONFIG ---------------- #
+MONGO_URL = os.getenv("MONGO_URL")
+DB_NAME = os.getenv("DB_NAME", "MovieBotDB")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "movies")
+
+# ---------------- REDIS CONFIG ---------------- #
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_USERNAME = os.getenv("REDIS_USERNAME", None)
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+
+# ---------------- LOGGING SETUP ---------------- #
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
+    datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        RotatingFileHandler(
-            "wroxenbot.txt",
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
+        RotatingFileHandler("bot.log", maxBytes=50_000_000, backupCount=10),
+        logging.StreamHandler(),
+    ],
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
